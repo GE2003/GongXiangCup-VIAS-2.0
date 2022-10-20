5031171c7a59ada929ecc401c0804490b00cb704
GE2003 <2470789323@qq.com>
2022-10-21 00:38:38 +0800

    shiyue1.0版本更新-首页和个人页面实现

--- app/src/main/java/com/example/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/View/Activity/MainActivity.java
@@ -1,17 +1,71 @@
 package com.example.View.Activity;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.example.Base.BaseFragment;
+import com.example.View.Fragment.HomeFragment;
+import com.example.View.Fragment.MineFragment;
+import com.example.View.Fragment.PersonalFragment;
 import com.example.shiyue.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HomeFragment homeFragment;
+    private PersonalFragment personalFragment;
+   @BindView(R.id.bottom_nav)
+    public BottomNavigationView navigationView;
+    private MineFragment mineFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        navigationView =this.findViewById(R.id.bottom_nav);
+        initFragment();
+        setOnChangeListener();
+    }
 
+    private void setOnChangeListener() {
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home_page:
+                        switchFragment(homeFragment);
+                        break;
+                    case R.id.my_page:
+                        switchFragment(mineFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initFragment() {
+        homeFragment = new HomeFragment();
+        personalFragment = new PersonalFragment();
+        mineFragment = new MineFragment();
+        switchFragment(homeFragment);
+    }
+
+    private void switchFragment(BaseFragment target) {
+        FragmentManager fm =getSupportFragmentManager();
+        FragmentTransaction ft = fm
+                .beginTransaction()
+                .setCustomAnimations(R.anim.from_right,R.anim.slide_left_out,R.anim.from_right,R.anim.slide_left_out);
+        ft.replace(R.id.fragment_container,target);
+        ft.commit();
     }
 }
--- app/src/main/java/com/example/View/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/View/Fragment/HomeFragment.java
@@ -2,11 +2,13 @@ package com.example.View.Fragment;
 
 import android.content.Context;
 import com.example.Base.BaseFragment;
+import com.example.shiyue.R;
 
 public class HomeFragment extends BaseFragment {
     @Override
     protected int getResId() {
-        return 0;
+
+        return R.layout.fragment_home;
     }
 
     @Override
